5ebb4a916a6cbd071d59649a2e1758db45195c2d
Prithvi <prithvis6002@gmail.com>
2026-04-03 22:36:48 +0530

    completion of frontend create page

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Title from "../components/Title";
 import UploadZone from "../components/UploadZone";
-import { RectangleVerticalIcon } from "lucide-react";
+import { Loader2Icon, RectangleHorizontalIcon, RectangleVerticalIcon, Wand2Icon } from "lucide-react";
+import { PrimaryButton } from "../components/Buttons";
 
 const Generator=() => {
     const[name,setName]=useState("");
     const[Productname,setProductName]=useState("");
     const[ProductDescription,setProductDescription]=useState("");
-    const[aspectRation,setAspectRation]=useState("9:16");
+    const[aspectRatio,setAspectRatio]=useState("9:16");
     const[productImage,setProductImage]=useState<File | null>(null);
     const[modelImage,setModelImage]=useState<File | null>(null);
     const[userprompt,setUserPrompt]=useState("");
@@ -44,7 +45,7 @@ const Generator=() => {
 
 
         <div className="w-full">
-           <div className="mb-4">
+           <div className="mb-4  text-gray-300">
             <label htmlFor="name" className="block text-sm mb-4"> Project Name</label>
             <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)}  placeholder="Name your project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm
             border-violet-200/10 focus:border-violet-500/50 outline-none transition-all" />
@@ -62,13 +63,33 @@ const Generator=() => {
            <div className="mb-4 text-gray-300">
             <label className="block text-sm mb-4"> Aspect Ratio</label>
             <div className="flex gap-3">
-                <RectangleVerticalIcon onClick={()=>setAspectRation('9:16')}
+                <RectangleVerticalIcon onClick={()=>setAspectRatio('9:16')}
                 className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
-                ring-transparent cursor-pointer ${aspectRatio==='9:16'? 'ring-violet-500/50 bg-white/10': ''`} />
+                ring-transparent cursor-pointer ${aspectRatio==='9:16'? 'ring-violet-500/50 bg-white/10': ' '}` }/>
+            <RectangleHorizontalIcon onClick={()=>setAspectRatio('16:9')}
+                className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
+                ring-transparent cursor-pointer ${aspectRatio==='16:9'? 'ring-violet-500/50 bg-white/10': ' '}` }/>
+           
             </div>
            </div>
+           <div className="mb-4 text-gray-300">
+            <label htmlFor="userPrompt" className="block text-sm mb-4">User Prompt <span className="text-xs text-violet-400">(optional)</span></label>
+            <textarea id="userPrompt" rows={4} value={userprompt} onChange={(e)=>setUserPrompt(e.target.value)} placeholder="Enter your prompt" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm
+            border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all" />
+           </div>
+           
         </div>
-    </div>   
+    </div> 
+    <div className="flex justify-center mt-10">
+        <PrimaryButton disabled={isGenerating}  className="px-10 py-3 rounded-md
+         disabled:opacity-70 disabled:cursor-not-allowed" >
+            {isGenerating? (<>
+            <Loader2Icon className=" size-5animate-spin" />Generating....
+            </>):(<>
+            <Wand2Icon className="size-5" />Generate Image </>)}
+        </PrimaryButton>
+
+        </div>  
    </form>
         </div>
     )
